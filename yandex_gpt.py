133d45aaaab6e999b61434eb73c05bb1f75e1fa7
Ryan Gosling <152086433+FreddyFazberUrUr@users.noreply.github.com>
2024-05-25 22:31:40 +0300

    Update yandex_gpt.py

--- yandex_gpt.py
+++ yandex_gpt.py
@@ -15,38 +15,46 @@ def count_gpt_tokens(messages):
         'Content-Type': 'application/json'
     }
     data = {
-        "modelUri": f"gpt://{FOLDER_ID}/{GPT_MODEL}",
+        'modelUri': f"gpt://{FOLDER_ID}/{GPT_MODEL}",
         "messages": messages
     }
     try:
-        response = requests.post(url=GPT_URL, json=data, headers=headers).json()["tokens"]
+        response = requests.post(url=GPT_URL, json=data, headers=headers).json()['tokens']
         return len(response)
     except Exception as e:
         logging.error(e)
         return 0
 
 
-def ask_gpt(messages):
+def ask_gpt(messages, level=1):
+    sys_text = SYSTEM_PROMPT[0]['text'].format(str(level))
+    print(sys_text)
     headers = {
-        "Authorization": f"Bearer {IAM_TOKEN}",
-        "Content-Type": "application/json"
+        'Authorization': f'Bearer {IAM_TOKEN}',
+        'Content-Type': 'application/json'
     }
     data = {
-        "modelUri": f"gpt://{FOLDER_ID}/{GPT_MODEL}",
+        'modelUri': f"gpt://{FOLDER_ID}/{GPT_MODEL}",
         "completionOptions": {
             "stream": False,
             "temperature": 0.7,
             "maxTokens": MAX_GPT_TOKENS
         },
-        "messages": SYSTEM_PROMPT + messages
+        "messages": [{'role': "system", 'text': sys_text}] + messages
     }
     try:
         response = requests.post(GPT_URL, headers=headers, json=data)
         if response.status_code != 200:
             return False, f"Ошибка GPT. Статус-код: {response.status_code}", None
-        answer = response.json()["result"]["alternatives"][0]["message"]["text"]
-        tokens_in_answer = count_gpt_tokens([{"role": "assistant", "text": answer}])
+        answer = response.json()['result']['alternatives'][0]['message']['text']
+        tokens_in_answer = count_gpt_tokens([{'role': 'assistant', 'text': answer}])
         return True, answer, tokens_in_answer
     except Exception as e:
         logging.error(e)
         return False, "Ошибка при обращении к GPT",  None
+
+
+if __name__ == '__main__':
+    ask_gpt([], 2)
+    ask_gpt([], 3)
+    ask_gpt([], 4)
